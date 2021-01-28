689bc4e91de15cca7a3a12e7e74c8d21d7a9e72d
deborathgascon <77179589+deborathgascon@users.noreply.github.com>
2021-01-28 10:13:43 -0500

    JS_Ej2_Operadores_Objeto

---
+++ JS_Ej2_Operadores/JS2.js
@@ -0,0 +1,9 @@
+const meses = ['Enero','Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+const Deborath = {
+    mes: 'octubre',
+    day: 06};
+
+console.log("Mi cumpleaños es en el mes de " + Deborath.mes + " el día " + Deborath.day);
+
+console.log(meses[10]);
