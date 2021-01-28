af75f503a8af628738cbb02f8a8c398cf1248e56
deborathgascon <77179589+deborathgascon@users.noreply.github.com>
2021-01-28 18:16:44 -0500

    JS_Ej3_Array

---
+++ JS_Ej3_Array/JS3.js
@@ -0,0 +1,12 @@
+const data = ['hola', 2, 5, 'adios'];
+
+// Determina cuál de los dos números que hay en el array es mayor.
+console.log('2 es mayor que 5', data [1]>data[2])
+console.log('5 es mayor que 2', data [2]> data[1])
+
+// Determina el resultado de las cinco operaciones numéricas realizadas con los dos elementos numéricos.
+console.log('suma', data[1]+data[2])
+console.log('resta', data[1]-data[2])
+console.log('multiplicación', data[1]*data[2])
+console.log('división', data[1]/data[2])
+console.log('resto división', data[1]%data[2])
