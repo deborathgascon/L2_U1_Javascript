e4200e19351d540640eb46ae0341ede856e63899
deborathgascon <77179589+deborathgascon@users.noreply.github.com>
2021-01-28 22:19:14 -0500

    JS_Ej9_Mayusculas

---
+++ JS_Ej9_Mayusculas/JS9.js
@@ -0,0 +1,7 @@
+const Topics = ['JavaScript', 'Variables', 'funciones', 'condicionales', 'bucles'];
+
+const MayuTopics = Topics.map (eachTopic => eachTopic.toUpperCase());
+MayuTopics.reverse();
+
+console.log ('Topics', Topics)
+console.log ('MayuTopics', MayuTopics)
